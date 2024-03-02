d02eac2bf252395797c3c8f99bf7089eb573a2f7
HieeusNguyen <hieudeptrai4603@gmail.com>
2024-03-02 21:50:44 +0700

    Refator code: create router, app/controllers folder

---
+++ src/app/controllers/NewsControllers.js
@@ -0,0 +1,13 @@
+class NewsControllers {
+    index(req, res) {
+        // [GET] / news
+        res.render("news");
+    }
+
+    // GET / news/:slug
+    show(req, res){
+        res.send('NEW DETAIL!')
+    }
+}
+
+module.exports = new NewsControllers;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
+const { send } = require("process");
 const app = express();
 const port = 3000;
+const route = require("./routes/");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan("combined"));
@@ -20,13 +29,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
-
-app.get("/news", (req, res) => {
-    res.render("news");
-});
+//Route Init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`); //http://localhost:3000/
